233e2a9191c63d1acce475e7f198cd2e565b00a5
carly-portch <carly.portch@outlook.com>
2025-01-30 18:08:22 +0800

    Update future_you.py

--- future_you.py
+++ future_you.py
@@ -3,6 +3,7 @@ import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
 from datetime import date
+import datetime
 
 # Set page config for better layout
 st.set_page_config(layout="wide")
